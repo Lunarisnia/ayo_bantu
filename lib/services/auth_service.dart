b3211a8c00c23a45cf99004a74799dfbf26cc49f
loona <r.arswendo.r@gmail.com>
2020-08-06 13:58:34 +0700

    [ADD] Basic Authentication

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,15 +2,20 @@ import 'package:ayo_bantu/constant/config.dart';
 import 'package:dio/dio.dart';
 
 Dio dio = Dio();
+
 class AuthService {
-  login(Map<String,dynamic> credential) async {
+  login(Map<String, dynamic> credential) async {
     try {
       final Response resp = await dio.post("$API/login", data: credential);
 
       return resp.data;
     } catch (e) {
-      print(e);
-      throw Exception("Error: A1");
+      if (e.response.statusCode == 401) {
+        throw Exception("Invalid email/password.");
+      } else {
+        print(e);
+        throw Exception("Error: A1");
+      }
     }
   }
 }
